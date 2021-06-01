a98e343ad78f8a3b842bd14ffee9372be48e3076
mikwl <jauntymike@gmail.com>
2021-06-01 23:17:23 +0300

    Create repo on GitHub.Upload 2nd task

---
+++ task_1.py
@@ -0,0 +1,3 @@
+"""As the first task you should use pet swagger:
+https://petstore.swagger.io/Endpoint - users.
+You need to write CRUD operation tests for testing this  endpoint."""
---
+++ task_3.py
@@ -0,0 +1,5 @@
+"""c)	For the third code task you should create UI tests for https://www.metric-conversions.org/ .
+You have to create the following tests:
+- Test for converting Celsius to Fahrenheit temperature;
+- Test for converting meters to feet;
+- Test for converting ounces to grams"""
